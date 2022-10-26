f53601e56864f5a249cd2cba2b9f85d03725e24d
Sasha <frolovd@vtb.ru>
2022-10-26 14:30:05 +0300

    Добавлен конструктор копирования

--- my_string.h
+++ my_string.h
@@ -9,9 +9,12 @@ using namespace std;
 class MyString {
 public:
 	MyString();
-	MyString(const char string[]);
+	MyString(const char String[]);
 	MyString(const char chr, ...);
 	MyString(string stroka);
+	MyString(const char String[], size_t count);
+	MyString(size_t count, const char sim);
+	MyString(const MyString& other);
 	size_t size();
 	size_t capacity();
 	void Print();
